27a0892292db6aafd0aa46cd88dc426e190bd9e4
Vincent <85881442+vincenz-cdf@users.noreply.github.com>
2023-03-27 18:43:33 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, url_for, request
+import googleapiclient.discovery
+import googleapiclient.errors
+
+app = Flask(__name__)
+
+def get_channel_id(api_key, channel_name_or_custom_url):
+    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=api_key)
+
+    request = youtube.search().list(
+        part="snippet",
+        type="channel",
+        q=channel_name_or_custom_url,
+        maxResults=1
+    )
+    response = request.execute()
+   
+    if response["items"]:
+        return response["items"][0]["snippet"]["channelId"]
+    else:
+        return None
+
+def get_channel_videos(api_key, channel_id):
+    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=api_key)
+
+    request = youtube.channels().list(
+        part="contentDetails",
+        id=channel_id
+    )
+    response = request.execute()
+    playlist_id = response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+
+    videos = []
+    next_page_token = None
+
+    while True:
+        request = youtube.playlistItems().list(
+            part="snippet",
+            playlistId=playlist_id,
+            maxResults=50,
+            pageToken=next_page_token
+        )
+        response = request.execute()
+        videos.extend(response["items"])
+
+        next_page_token = response.get("nextPageToken")
+
+        if next_page_token is None:
+            break
+
+    return videos
+
+def get_video_titles(videos):
+    video_titles = []
+
+    for video in videos:
+        video_titles.append(video["snippet"]["title"])
+
+    return video_titles
+
+def get_video_titles_and_urls(videos):
+    video_data = []
+
+    for video in videos:
+        video_title = video["snippet"]["title"]
+        video_url = f"https://www.youtube.com/watch?v={video['snippet']['resourceId']['videoId']}"
+        video_data.append({"title": video_title, "url": video_url})
+
+    return video_data
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    output = request.form.to_dict()
+    print(output)
+    name = output["name"]
+
+    api_key = "AIzaSyDyaLoyoR_Hlnja0akVPGXORsC8kuFe3A0"
+    channel_id = get_channel_id(api_key, name)
+
+    if channel_id:
+        videos = get_channel_videos(api_key, channel_id)
+        video_data = get_video_titles_and_urls(videos)
+        video_data.reverse()
+    else:
+        video_data = None
+
+    return render_template('index.html', name=name, video_data=video_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
